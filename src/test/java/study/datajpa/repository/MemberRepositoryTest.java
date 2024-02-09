399501146ea6e116f3a960d2555e7803f3b657f0
iamminseongKim <minseongkimim@gmail.com>
2024-02-09 14:18:17 +0900

    섹션 1

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("")
+    void testMember() throws Exception {
+        //given
+        Member member = new Member("memberA");
+
+        //when
+        Member savedMember = memberRepository.save(member);
+        Member findMember= memberRepository.findById(savedMember.getId()).get();
+        //then
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);   // findMember==member
+    }
+}
