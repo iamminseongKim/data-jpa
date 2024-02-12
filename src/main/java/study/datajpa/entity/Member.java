c50dfc3a7a475c9a88660dd45f57790c7b829bce
iamminseongKim <minseongkimim@gmail.com>
2024-02-12 12:36:49 +0900

    섹션 4 - 쿼리 메서드 기능 - 파라미터 바인딩

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -10,6 +10,10 @@ import static lombok.AccessLevel.*;
 @Getter @Setter
 @NoArgsConstructor(access = PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name="Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
@@ -18,6 +22,10 @@ public class Member {
     private String username;
     private int age;
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "team_id")
