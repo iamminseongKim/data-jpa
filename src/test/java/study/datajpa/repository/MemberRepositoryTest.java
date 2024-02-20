1881dd5890d81ecae355ccb8d6072e93d81a0736
kms <kms@tigensoft.co.kr>
2024-02-21 08:27:31 +0900

    섹션 7 - 나머지 기능들 - 完

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -380,6 +380,29 @@ class MemberRepositoryTest {
     }
 
 
+    @Test
+    public void nativeQueryTest() throws Exception {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+        //when
+        Page<MemberProjection> result = memberRepository.findByNativeProjections(PageRequest.of(0, 10));
+        //then
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println(memberProjection.getUsername());
+            System.out.println(memberProjection.getTeamName());
+        }
+
+    }
 
 
 }
